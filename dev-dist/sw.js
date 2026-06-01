004756422baa9065a1a6c5022964e464ce8de4a5
Miguel tite <matttite24@gmail.com>
2026-06-01 18:42:19 -0500

    fix: revertir a stdin para imprimir, mantener CORS

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tj2ig3rdqpo"
+    "revision": "0.tl35m260i"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
