d107ca4515c5d050eebc42649de393b21fe81e6b
Miguel tite <matttite24@gmail.com>
2026-06-01 15:46:17 -0500

    feat: embed print server as sidecar in Tauri app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,23 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+use tauri_plugin_shell::ShellExt;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                handle
+                    .shell()
+                    .sidecar("pos-print-server")
+                    .expect("pos-print-server sidecar not found")
+                    .spawn()
+                    .expect("failed to spawn pos-print-server");
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
